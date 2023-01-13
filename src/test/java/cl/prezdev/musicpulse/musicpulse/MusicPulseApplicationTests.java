050ba9fd66e645c5d2df59d303f71f31b3afc40c
pperezp <patodeath@gmail.com>
2023-01-13 01:54:40 -0300

    feat: initial commit (mysql configuration in application.properties)

---
+++ src/test/java/cl/prezdev/musicpulse/musicpulse/MusicPulseApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.prezdev.musicpulse.musicpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicPulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
